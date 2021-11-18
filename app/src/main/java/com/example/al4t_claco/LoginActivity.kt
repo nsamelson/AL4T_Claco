a046ca31902e670d5969eb7f14b66b76521193f1
AmineAousji <aousjimedamine9@gmail.com>
2021-11-19 00:30:41 +0100

    Login commit

---
+++ app/src/main/java/com/example/al4t_claco/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.al4t_claco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
