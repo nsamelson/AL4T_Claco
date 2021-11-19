62b564b0f5c5f107a9d32420fe4815414d5f523c
wasanico <salocin.sam@gmail.com>
2021-11-19 11:41:49 +0100

    added resource page

--- app/src/main/java/com/example/al4t_claco/MainActivity.kt
+++ app/src/main/java/com/example/al4t_claco/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
 
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.resources)
 
         //--------------TEST ZONE
         /*
