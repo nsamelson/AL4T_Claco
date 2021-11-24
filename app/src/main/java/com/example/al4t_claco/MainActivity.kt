80891789ca9c288e08348b1c24d8dfec271bf604
Nicolas Samelson <salocin.sam@gmail.com>
2021-11-24 19:56:00 +0100

    dynamically added buttons, need further improvements

--- app/src/main/java/com/example/al4t_claco/MainActivity.kt
+++ app/src/main/java/com/example/al4t_claco/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.al4t_claco
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.gridlayout.widget.GridLayout
 import com.example.al4t_claco.databinding.ResourcesBinding
 import com.example.al4t_claco.view.DataActivity
 
@@ -25,10 +29,34 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.title = "Resources"
 
 
+        fun addButtons() {
+            val gridlayout = findViewById<GridLayout>(R.id.gridResources)
+            //val buttonParams = findViewById<Button>(R.id.button1)
+            for(i in 1..5){
+                val newButton = Button(this)
+                newButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.pdf_medium, 0, 0);
+
+                /*newButton.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT)*/
+
+                newButton.setOnClickListener(View.OnClickListener {
+                    newButton.text = "clicked"
+                })
+                newButton.text = "Button ${i}"
+                newButton.id = i
+                gridlayout.addView(newButton)
+            }
+
+        }
+        addButtons()
+
+
 
 
 
 
 
     }
+
 }
