7c9ab79d9f9739bba331ff17db27cb9b6734ee11
Nicolas Samelson <salocin.sam@gmail.com>
2021-11-25 15:10:54 +0100

    tuned button animations, added popup

--- app/src/main/java/com/example/al4t_claco/File.kt
+++ app/src/main/java/com/example/al4t_claco/File.kt
@@ -1,6 +1,6 @@
 package com.example.al4t_claco
 
 class File (name: String, type: String){
-    var Name = name
-    var Type = type
+    var name = name
+    var type = type
 }
--- app/src/main/java/com/example/al4t_claco/MainActivity.kt
+++ app/src/main/java/com/example/al4t_claco/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.al4t_claco
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.gridlayout.widget.GridLayout
@@ -15,41 +21,70 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.resources)
+        //setContentView(R.layout.activity_main)
 
         //TODO: method when an activity is selected
 
         val teachers = listOf("Lorge","Lurkin","Dekimpe")
         val activity = Activity("Informatique","4inf",teachers)
-        activity.resources = listOf(File("a","pdf"),File("b","pdf"))
+        activity.resources = listOf(File("a","pdf"),File("b","pdf"), File("c","PDF"),File("d","text"))
 
 
         val binding: ResourcesBinding = DataBindingUtil.setContentView(this, R.layout.resources)
-        binding.resource = DataActivity(activity, "info")
+        binding.activity = DataActivity(activity, "info")
         supportActionBar?.title = "Resources"
 
 
-        fun addButtons() {
+        fun showResources(files: List<File>) {
             val gridlayout = findViewById<GridLayout>(R.id.gridResources)
-            //val buttonParams = findViewById<Button>(R.id.button1)
-            for(i in 1..5){
-                val newButton = Button(this)
-                newButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.pdf_medium, 0, 0);
+            fun createAndShowDialog(file: File){
 
-                /*newButton.layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT)*/
+                val dialogBuilder = AlertDialog.Builder(this)
+                dialogBuilder.setTitle("Do you want to open the following file?")
+                dialogBuilder.setMessage("Name : ${file.name}\nType : ${file.type}")
+                dialogBuilder.setNegativeButton("Cancel",
+                    DialogInterface.OnClickListener { dialog, whichButton -> })
+                dialogBuilder.setNeutralButton("Download",
+                    DialogInterface.OnClickListener { dialog, whichButton ->
+                        Toast.makeText(applicationContext,
+                            "Downloading", Toast.LENGTH_SHORT).show()
+                    })
+                dialogBuilder.setPositiveButton("Open",
+                    DialogInterface.OnClickListener { dialog, whichButton -> })
 
+                val b = dialogBuilder.create()
+                b.show()
+
+            }
+            for(file in files){
+                val newButton = Button(this,null, android.R.attr.borderlessButtonStyle)
+
+                val buttonImage : Int
+                when (file.type){
+                    "pdf" -> buttonImage = R.drawable.pdf_medium
+                    else  -> buttonImage = R.drawable.not_found_medium
+                }
+                newButton.setCompoundDrawablesWithIntrinsicBounds(0, buttonImage, 0, 0)
+                newButton.text = file.name
+
+                //On short click : show file
                 newButton.setOnClickListener(View.OnClickListener {
-                    newButton.text = "clicked"
+                    //TODO("Show file")
+                    newButton.text = "show file"
                 })
-                newButton.text = "Button ${i}"
-                newButton.id = i
+                //On long click : popup to download file
+                newButton.setOnLongClickListener(View.OnLongClickListener {
+                    newButton.text = file.name
+                    createAndShowDialog(file)
+                    true
+                })
+
                 gridlayout.addView(newButton)
             }
 
+
         }
-        addButtons()
+        showResources(activity.resources)
 
 
 
