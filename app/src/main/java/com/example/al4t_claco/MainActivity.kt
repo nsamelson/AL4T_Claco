ceab9cc76faad0fdc70b18e4ce4a631e6ff3339e
Dari76 <pierrepapillon76@hotmail.fr>
2021-11-26 11:41:35 +0100

    edit course information page

--- app/src/main/java/com/example/al4t_claco/MainActivity.kt
+++ app/src/main/java/com/example/al4t_claco/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.al4t_claco.controller.CourseInformation
 import com.example.al4t_claco.controller.ResourceActivity
 import com.example.al4t_claco.model.Activity
+import com.example.al4t_claco.model.Course
 import com.example.al4t_claco.model.File
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +20,27 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    fun openCourseInformation(view: View):Intent{
+        val activity = Activity("Info","4inf", listOf("Lorge","Lurkin","Dekimpe"))
+        val activity2 = Activity("Bidule","4inf", listOf("Lorge","Lurkin","Dekimpe"))
+        val activity3 = Activity("Truc","4inf", listOf("Lorge","Lurkin","Dekimpe"))
+        val activity4 = Activity("Nom très très long parce que les cours sont chiants","4inf", listOf("Lorge","Lurkin","Dekimpe"))
+        val activity5 = Activity("Informatique","4inf", listOf("Lorge","Lurkin","Dekimpe"))
+        val activity6 = Activity("Informatique","4inf", listOf("Lorge","Lurkin","Dekimpe"))
+        val activityList = listOf<Activity>(activity,activity2,activity3,activity4,activity5,activity6,activity4,activity4,activity4)
+        val course = Course("GPU_Computing","35foezkg",2021,6,"Lurkin", activityList)
+
+        val intent = Intent(this, CourseInformation::class.java).apply {
+            putExtra("course",course)
+        }
+        startActivity(intent)
+        return intent
+    }
+
+
     fun openResourceActivity(view: View):Intent{
-        val activity = Activity("Informatique","4inf",listOf("Lorge","Lurkin","Dekimpe"))
+        val activity = Activity("Informatique","4inf", listOf("Lorge","Lurkin","Dekimpe"))
         activity.resources = listOf(File("a","pdf"), File("b","pdf"), File("c","PDF"), File("d","text"))
 
         val intent = Intent(this, ResourceActivity::class.java).apply {
